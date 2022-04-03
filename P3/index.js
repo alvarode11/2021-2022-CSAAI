07b19214cb604a1f193e86bf489b1c3de765854d
alvarode11 <alvarodominguezerrejon@gmail.com>
2022-04-03 20:12:33 +0200

    mejoras se sup0ne y ya PARA DUDAS PROFE

--- P3/index.js
+++ P3/index.js
@@ -1,3 +1,5 @@
+//Definiendo todas las variables y constantes que voy a utilizar para la practica
+
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
@@ -8,20 +10,26 @@ const musica = new Audio('pum.mp3');
 const MUSICA = new Audio('gameover.mp3');
 const MUSIC = new Audio('error.mp3');
 var startGame = false;
-
 let score = 0;
-
+let microsegundos = 0;
+let segundos = 0;
+let minutos = 0;
+let vidas = 3;
 const brickRowCount = 9;
 const brickColumnCount = 5;
 
+//Creando pelotita
+
 const ball = {
     x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 10,
+    y: canvas.height / 2 +270,
+    size: 15,
     speed: 4,
-    dx: 4,
+    dx: 1.5,
     dy: -4
 };
+
+//Creando la raqueta
 
 const paddle = {
     x: canvas.width / 2 -40,
@@ -32,6 +40,8 @@ const paddle = {
     dx: 0
 };
 
+//Definiendo caracteristicas de cada
+
 const brickInfo = {
     w: 70,
     h: 20,
@@ -40,6 +50,8 @@ const brickInfo = {
     offsetY: 60,
     visible: true
 };
+
+//Creando los ladrillos
 
 const bricks = [];
 for (let i = 0; i < brickRowCount; i++){
@@ -51,26 +63,41 @@ for (let i = 0; i < brickRowCount; i++){
     }
 }
 
+//Dibujando la pelotita dentro del canvas
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-    ctx.fillStyle = '#48d1cc';
+    ctx.fillStyle = '#3399FF';
     ctx.fill();
-    ctx.closePath()
+    ctx.closePath();
 }
 
-var vidas = 3;
+//Dibujando la raqueta dentro del canvas
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = '#00FFCC';
+    ctx.fill();
+    ctx.closePath();
+}
+
+//Dibujando la puntuacion dentro del canvas
+
+function drawScore(){
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}` , canvas.width - 100, 30);
+}
+
+//Dibujando las vidas dentro del canvas
 
 function drawVidas() {
     ctx.font = '20px Arial';
     ctx.fillText(`Vidas: ${vidas}`, canvas.width-750, 30);
 }
 
-
-
-let microsegundos = 0;
-let segundos = 0;
-let minutos = 0;
+//Funcion que calculara el tiempo 
 
 function time(){
     microsegundos ++;
@@ -93,40 +120,35 @@ function time(){
     }
 
 }
+
+//Dibujando el cronometro dentro del canvas
+
 function cronometro(){
     ctx.font = "20px Arial";
     ctx.fillText('Tiempo: ', 355, 30);
     ctx.fillText(minutos + ':' + segundos, 435, 30);
 }
 
-
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-    ctx.fillStyle = '#48d1cc';
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawScore(){
-    ctx.font = '20px Arial';
-    ctx.fillText(`Score: ${score}` , canvas.width - 100, 30);
-}
+//Dibujando los ladrillos dentro del canvas
 
 function drawBricks(){
     bricks.forEach(column => {
         column.forEach(brick => {
             ctx.beginPath();
             ctx.rect(brick.x, brick.y, brick.w, brick.h);
-            ctx.fillStyle = brick.visible ? '#48d1cc' : 'transparent';
+            ctx.fillStyle = brick.visible ?'#00FFCC': 'transparent';
             ctx.fill();
             ctx.closePath();
         });
     });
 }
 
+//Movimiento de la raqueta
+
 function movePaddle() {
     paddle.x += paddle.dx;
+
+    //Detecta las paredes del canvas
 
     if(paddle.x + paddle.w > canvas.width){
         paddle.x = canvas.width - paddle.w;
@@ -137,43 +159,38 @@ function movePaddle() {
     }
 }
 
-function moveBall() {
+//Movimiento de la pelotita en el canvas
 
-    if(startGame){
-        ball.x += ball.dx;
-        ball.y += ball.dy;
+function moveBall() {
+    if (startGame){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
     } 
+
+    //Colision con las paredes laterales del canvas
+
     if(ball.x + ball.size > canvas.width || ball.x - ball.size < 0){
-        ball.dx *= -1;
+        ball.dx *= -1; 
     }
 
-    if(ball.y + ball.size > canvas.height ) {
+    //Colision con el suelo y el techo del canvas
 
+    if(ball.y + ball.size > canvas.height || ball.y - ball.size <0){
         ball.dy *= -1; 
-        vidas = vidas - 1;
-        MUSIC.play();
-   
-        if(vidas ==0){
-
-            MUSICA.play();
-            alert('GAME OVER')
-                        document.location.reload();
-        }
-
-    }else if(ball.y - ball.size <0){
-
-        ball.dy *= -1; 
-        
     }
     
+    //Colision con la raqueta
+
     if(
         ball.x - ball.size > paddle.x &&
         ball.x + ball.size < paddle.x + paddle.w &&
         ball.y + ball.size > paddle.y
     ){
         ball.dy = -ball.speed;
-        music.play();
+        MUSIC.play();
     }
+
+    //Colision con los ladrillos
 
     bricks.forEach(column => {
         column.forEach(brick => {
@@ -193,11 +210,24 @@ function moveBall() {
         });
     });
 
+    //Si la pelotita toca el suelo pierdes
+
     if(ball.y + ball.size > canvas.height) {
+        vidas = vidas - 1;
+        music.play();
+        if(vidas ==0){
+
+            MUSICA.play();
+            alert('GAME OVER')
+                        document.location.reload();
+        }
         showAllBricks();
         score = 0;
+
     }
 }
+
+//Funcion para la puntuacion
 
 function increaseScore() {
     score++;
@@ -207,14 +237,18 @@ function increaseScore() {
     }
 }
 
+//Muestra todos los ladrillos a la vez
+
 function showAllBricks() {
     bricks.forEach(column => {
         column.forEach(brick => (brick.visible = true));
     });
 }
 
-function draw() {
+//Procedemos a dibujar todo lo anterior en el canvas
 
+function draw() {
+   
     ctx.clearRect(0,0,canvas.width, canvas.height);
 
     drawBall();
@@ -224,6 +258,8 @@ function draw() {
     drawVidas();
     cronometro();
 }
+
+//Funcion para todo aquello que no es constante y necesita de actualizarse
 
 function update() {
     movePaddle();
@@ -235,6 +271,7 @@ function update() {
 
 update();
 
+//Evento al presionar una tecla
 
 function Keydown(ev) {
     switch (ev.keyCode) {
@@ -259,6 +296,8 @@ function Keydown(ev) {
     }
 }
 
+//Evento al dejar de presionar una tecla
+
 function Keyup(ev){
 
     switch (ev.keyCode) {
@@ -279,9 +318,12 @@ function Keyup(ev){
 
 }
 
+//Llamando a los eventos del teclado
+
 document.addEventListener('keydown', Keydown);
 document.addEventListener('keyup', Keyup);
 
+//Eventos para los botones de las reglkas que se mostraran por pantalla
 
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
 closeBtn.addEventListener('click', () => rules.classList.remove('show'));
